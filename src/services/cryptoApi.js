022d4d943a20b4f7f8f551a2c4455aaf1d51503a
Mike Thompson <mtthompson11@gmail.com>
2022-08-17 00:54:25 -0400

    first commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '5036e68de7mshfe82b9b0ebf4ee7p1ee324jsnc03e78fe98a6',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi;
